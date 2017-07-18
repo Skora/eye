416489338efd87ca903d85e380931fcd9d872c20
Skorzecki Pawel <pawel.skorzecki@tieto.com>
2017-07-18 10:35:41 +0200

    cleaned repository

--- FunVision/Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lopencv_objdetect -lopencv_core -lopencv_videoio -lopencv_imgcodecs -lopencv_highgui -lopencv_imgproc
-
