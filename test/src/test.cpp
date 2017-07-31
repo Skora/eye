b3aa7296f89a99f6ad618d47c827d7cb548c27c7
Skorzecki Pawel <pawel.skorzecki@tieto.com>
2017-07-31 16:46:04 +0200

    added cjsos lib

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -7,6 +7,8 @@
 //============================================================================
 
 #include "program_options.hpp"
+#include "cJSON.h"
+#include <iostream>
 
 using namespace std;
 
@@ -24,6 +26,20 @@ int main(int argc, char* argv[]) {
 	progOptions.addOption("--h", &help);
 	progOptions.addOption("--help", &help);
 	progOptions.executeOptions(argc, argv);
+
+	cJSON *root;
+	cJSON *fmt;
+	root = cJSON_CreateObject();
+	cJSON_AddItemToObject(root, "name", cJSON_CreateString("Jack (\"Bee\") Nimble"));
+	cJSON_AddItemToObject(root, "format", fmt = cJSON_CreateObject());
+	cJSON_AddStringToObject(fmt, "type", "rect");
+	cJSON_AddNumberToObject(fmt, "width", 1920);
+	cJSON_AddNumberToObject(fmt, "height", 1080);
+	cJSON_AddFalseToObject (fmt, "interlace");
+	cJSON_AddNumberToObject(fmt, "frame rate", 24);
+	char *rendered = cJSON_Print(root);
+	cJSON_Delete(root);
+	cout << rendered << endl;
 
 
 
