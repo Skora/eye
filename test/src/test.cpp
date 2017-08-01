6eda7d21301b711b465a14bbd5247f5963c4159c
Skorzecki Pawel <pawel.skorzecki@tieto.com>
2017-08-01 15:27:21 +0200

    work on json generation

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -6,40 +6,85 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
+#include <iostream>
+
 #include "program_options.hpp"
 #include "cJSON.h"
-#include <iostream>
+#include <cstring>
+#include <stdlib.h>
+
+
 
 using namespace std;
 
-
+#define SERIAL_ID 123456
 
 ProgramOptions progOptions;
+bool debug_flag = false;
 
 void help (void)
 {
 	progOptions.listOptions();
 }
 
+void debug (void)
+{
+	debug_flag = true;
+}
+
+static inline char* createRegJson(int serialID)
+{
+	cJSON *root;
+	cJSON *pl;
+	char* msg;
+	root = cJSON_CreateObject();
+	cJSON_AddItemToObject(root, "top", cJSON_CreateString("register"));
+	cJSON_AddItemToObject(root, "pl", pl = cJSON_CreateObject());
+	cJSON_AddStringToObject(pl, "type", "eye_module");
+	cJSON_AddNumberToObject(pl, "sub_id", serialID);
+	cJSON_AddNumberToObject(pl, "pub_id", serialID);
+	cJSON_AddNumberToObject(root, "act", 1);
+	msg = cJSON_Print(root);
+	cJSON_Delete(root);
+	return msg;
+}
+
+static inline char* addJsonLength(char *jsonMsg, int length)
+{
+	#define LEN_BYTES 4U
+	char* newBuf;
+
+    newBuf = (char*)malloc(LEN_BYTES + length);
+
+  //  length = htonl((unsigned long)length); // make it work
+    memcpy(newBuf, (char*)&length, LEN_BYTES);
+    memcpy(newBuf + LEN_BYTES, jsonMsg, length);
+
+    return newBuf;
+}
+
+
 int main(int argc, char* argv[]) {
+
+
 
 	progOptions.addOption("--h", &help);
 	progOptions.addOption("--help", &help);
+	progOptions.addOption("--debug", &debug);
 	progOptions.executeOptions(argc, argv);
 
-	cJSON *root;
-	cJSON *fmt;
-	root = cJSON_CreateObject();
-	cJSON_AddItemToObject(root, "name", cJSON_CreateString("Jack (\"Bee\") Nimble"));
-	cJSON_AddItemToObject(root, "format", fmt = cJSON_CreateObject());
-	cJSON_AddStringToObject(fmt, "type", "rect");
-	cJSON_AddNumberToObject(fmt, "width", 1920);
-	cJSON_AddNumberToObject(fmt, "height", 1080);
-	cJSON_AddFalseToObject (fmt, "interlace");
-	cJSON_AddNumberToObject(fmt, "frame rate", 24);
-	char *rendered = cJSON_Print(root);
-	cJSON_Delete(root);
-	cout << rendered << endl;
+
+	char* regMsg = createRegJson(SERIAL_ID);
+	char* msg =  addJsonLength(regMsg, strlen(regMsg));
+
+	for (unsigned int i = 0; i <= 4; i++) 	//this is just a work around to visualize output
+	{										// cout sends strings not bytes so we send 4 length bytes first and then the rest
+		cout << (msg+(i*sizeof(char))) << endl;
+	}
+	//Absolutely necessary
+	free(regMsg);
+	free(msg);
+
 
 
 
